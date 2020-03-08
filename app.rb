2e64f9d54eca3f2308def63f360ab69c6e7a3990
Khalid T <khalid.tissire@gmail.com>
2020-03-08 09:29:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,9 @@
-require_relative 'config/environment'
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
+  end
 
-class App < Sinatra::Base
 end
